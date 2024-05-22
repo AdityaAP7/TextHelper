718120fba0370ac6053413df768fd1cd1fe4b538
Aditya Palodkar <96766033+AdityaAP7@users.noreply.github.com>
2024-05-22 23:32:53 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,8 +10,7 @@ export default function Navbar(props) {
      
 
 
-                    input className="form-check-input" onClick={props.toggleMode} type="checkbox" id="flexSwitchCheckDefault"/>
-                        <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Enable DarkMode</label>
+                   
 
                         input className="form-check-input" onClick={props.toggleMode} type="checkbox" id="flexSwitchCheckDefault"/>
                         <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Enable DarkMode</label>
